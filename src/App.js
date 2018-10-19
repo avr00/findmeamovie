4de068ebdaceb03e21ae71033547399c0cd53261
Avr <avrivadeneira1@gmail.com>
2018-10-19 06:15:44 -0300

    added react-router-dom to add a movie page to display more information about the selected movie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./header/Header";
 import Main from "./main/Main";
+import Movie from "./movie/Movie";
+import NotFound from "./NotFound";
 
 const App = () => {
   console.log("API key:", process.env.REACT_APP_TMDB_API_KEY);
   return (
-    <div>
-      <Header />
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/movies/:movieId" component={Movie} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./MovieListItem.scss";
+import { Link } from "react-router-dom";
 
 const MovieListItem = props => {
-  const { title, poster_path, release_date, vote_average } = props.movie;
+  const { id, title, poster_path, release_date, vote_average } = props.movie;
   const imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`;
   const year = release_date.substring(0, 4);
   return (
     <li className="movie-item">
-      <img src={imgUrl} alt="movieImage" />
+      <Link to={`/movies/${id}`} className="thumbnail">
+        <img src={imgUrl} alt="movieImage" />
 
-      <div className="movie-description">
-        <h2>{title}</h2>
+        <div className="movie-description">
+          <h2>{title}</h2>
 
-        <section className="movie-details">
-          <div className="movie-year">
-            <span className="title">Year</span>
-            <span>{year}</span>
-          </div>
+          <section className="movie-details">
+            <div className="movie-year">
+              <span className="title">Year</span>
+              <span>{year}</span>
+            </div>
 
-          <div className="movie-rating">
-            <span className="title">Rating</span>
-            <span>{vote_average}</span>
-          </div>
-        </section>
-      </div>
+            <div className="movie-rating">
+              <span className="title">Rating</span>
+              <span>{vote_average}</span>
+            </div>
+          </section>
+        </div>
+      </Link>
     </li>
   );
 };
